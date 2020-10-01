b88286615e980fb6734c185645d0c9e985577189
goffjira <goffjira@gmail.com>
2020-10-01 08:00:21 +0700

    Add Header with app title & Dropdown countries(hard coded)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { FormControl, MenuItem, Select } from "@material-ui/core";
 import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h2>Covid 19 Tracker</h2>
+      <div className="app__header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app__form">
+          <Select variant="outlined" value="Test">
+            <MenuItem value="Thailand">Thailand</MenuItem>
+            <MenuItem value="Netherland">Netherland</MenuItem>
+            <MenuItem value="Singapore">Singapore</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
 
       {/* Header */}
       {/* Title + Dropwdown Country */}
+
       {/* InfoBox */}
       {/* InfoBox */}
       {/* InfoBox */}
