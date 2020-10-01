24885b23395a2ec4f0f21580372dee54afd5637f
goffjira <goffjira@gmail.com>
2020-10-01 07:32:40 +0700

    Plan ahead all compoents in the app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="app">
       <h2>Covid 19 Tracker</h2>
+
+      {/* Header */}
+      {/* Title + Dropwdown Country */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* InfoBox */}
+      {/* Map */}
+      {/* InfoTable */}
+      {/* Graph */}
     </div>
   );
 }
